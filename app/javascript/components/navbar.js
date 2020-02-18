a9a4ae9f4563ad322f1843ab95d06811efb0fff1
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-02-18 10:44:14 +0100

    css banner footer ok

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,8 +1,8 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.getElementById('test');
+  const navbar = document.getElementById('navbar-scroll');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 470) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
