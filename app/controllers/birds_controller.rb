8843dcbe2d966e28fc9a82bab880febdd367ed1e
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-02-18 12:18:46 +0100

    debut show css + ajout description birds + seed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,15 +1,8 @@
 class BirdsController < ApplicationController
-  # before_action :set_bird, only: [:show]
-
   def index
   end
 
   def show
-  end
-
-  private
-
-  def set_bird
     @bird = Bird.find(params[:id])
   end
 end
