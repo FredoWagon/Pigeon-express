6016feff85bd416737442415a8357cecc23dcbe6
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-02-19 14:51:19 +0100

    footer ok

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.getElementById('navbar-scroll');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 470) {
+      if (window.scrollY >= 400) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
