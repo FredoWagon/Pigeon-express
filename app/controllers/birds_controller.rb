6d6a78b9ac6bf5c84bc6069c4192be784e9aca21
Frédéric Legrand <fedde.leg@gmail.com>
2020-02-19 14:59:20 +0100

    bird-creation

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,4 +8,25 @@ class BirdsController < ApplicationController
   def show
     @bird = Bird.find(params[:id])
   end
+
+  def new
+    @bird = Bird.new
+  end
+
+  def create
+    @bird = Bird.new(bird_params)
+    @bird.user_id = current_user[:id]
+    if @bird.save
+      redirect_to birds_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def bird_params
+    params.require(:bird).permit(:category, :name, :species, :range, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'birds#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :birds, only: [:index, :show] do
+  resources :birds, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:new, :show, :create]
   end
   resources :bookings, only: [:index]
