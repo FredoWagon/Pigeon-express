6fc83280a52285ac2e773ca98b19734af131f380
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-02-19 11:46:08 +0100

    map + markers + zoom

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,6 +3,15 @@ class BirdsController < ApplicationController
 
   def index
     @birds = Bird.all
+
+    @birds_geo = Bird.geocoded #returns birds with coordinates
+
+    @markers = @birds_geo.map do |bird|
+      {
+        lat: bird.latitude,
+        lng: bird.longitude
+      }
+    end
   end
 
   def show
