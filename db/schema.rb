006cd93906156007cc8a413f2fceed48cbfbeafb
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-02-19 11:26:50 +0100

    gemfile + migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_101558) do
+ActiveRecord::Schema.define(version: 2020_02_19_102359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_02_19_101558) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_birds_on_user_id"
   end
 
