a8dc0562ebb6cb4e1883fa87181c164f2dbe273d
Antoine Koenig <antoine24240@gmail.com>
2020-02-19 11:46:21 +0100

    Pas de login dans bird#index et bird#show

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @birds = Bird.all
